904f7ea4d589b0060085d149ed59add9cd27661f
jasmin261098 <hundtjasmin98@gmail.com>
2025-02-19 15:08:24 +0100

    Chrome Dinosaur Frame Construction

--- src/App.java
+++ src/App.java
@@ -2,6 +2,15 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        //create window
+        int boardWidth = 750;
+        int boardHeight = 250;
+
+        JFrame chromeDinosaurFrame = new JFrame("Chomis Jumping Adventure");
+        chromeDinosaurFrame.setVisible(true);
+        chromeDinosaurFrame.setSize(boardWidth, boardHeight);
+        chromeDinosaurFrame.setLocationRelativeTo(null);
+        chromeDinosaurFrame.setResizable(false);
+        chromeDinosaurFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
