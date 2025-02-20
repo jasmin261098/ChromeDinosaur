41c7daa1e410b2d7f379d0e4ffbe5363d10d5aca
jasmin261098 <hundtjasmin98@gmail.com>
2025-02-20 13:55:49 +0100

    Game Loop created

--- src/ChromeDinosaur.java
+++ src/ChromeDinosaur.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 import java.util.ArrayList; //to store all cactus
 import javax.swing.*;
 
-public class ChromeDinosaur extends JPanel {
+public class ChromeDinosaur extends JPanel implements ActionListener {
     int boardWidth = 750;
     int boardHeight = 250;
 
@@ -40,19 +40,42 @@ public class ChromeDinosaur extends JPanel {
 
     Block dinosaur;
 
+    Timer gameLoop;
+
+
     public ChromeDinosaur() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.lightGray);
 
         //load images
         dinosaurImg = new ImageIcon(getClass().getResource("./img/dino-run.gif")).getImage();
-        dinosaurImg = new ImageIcon(getClass().getResource("./img/dino-dead.png")).getImage();
-        dinosaurJumpImg = new ImageIcon(getClass().getResource("./img/dino-jump.img")).getImage();
-        cactus1Img = new ImageIcon(getClass().getResource("./img/cactus1.img")).getImage();
+        dinosaurDeadImg = new ImageIcon(getClass().getResource("./img/dino-dead.png")).getImage();
+        dinosaurJumpImg = new ImageIcon(getClass().getResource("./img/dino-jump.png")).getImage();
+        cactus1Img = new ImageIcon(getClass().getResource("./img/cactus1.png")).getImage();
         cactus2Img = new ImageIcon(getClass().getResource("./img/cactus2.png")).getImage();
         cactus3Img = new ImageIcon(getClass().getResource("./img/cactus3.png")).getImage();
 
         //dinosaur
         dinosaur = new Block(dinosaurX, dinosaurY, dinosaurWidth, dinosaurHeight, dinosaurImg);
+
+        //game loop
+        gameLoop = new Timer(1000/60, this); //1000/60 = 60 frames per 1000ms (1s), update
+        gameLoop.start();
+    }
+
+    //draw images on the Panel
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(dinosaurImg, dinosaur.x, dinosaur.y, dinosaur.width, dinosaur.height, null);
+    }
+
+    //game loop action listener recalling draw
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
     }
 }
